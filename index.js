c4a46d64c405a89116f13f2b4cd61f14dd5e5b0a
Kevin Jan <kejan@cisco.com>
2019-10-21 16:38:18 -0700

    earthquake simulator

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 let windowHeight = window.innerHeight
+let box = document.querySelector('#box1')
+let boxPosition = box.offsetTop + box.offsetHeight
+let animation = 'fade-up'
 
 document.addEventListener('scroll', () => {
-  let box = document.querySelector('#box1')
-  let boxPosition = box.getBoundingClientRect().top
-
-  console.log('Distance from top is:', boxPosition, 'Window height is: ', windowHeight)
-
-  boxPosition <= windowHeight - 100 && box.classList.add('fade')
-  boxPosition > windowHeight && box.classList.remove('fade')
+  if (window.pageYOffset + windowHeight < boxPosition) {
+    box.classList.remove(animation)
+    box.classList.add(`${animation}-opposite`)
+  } else if (window.pageYOffset + windowHeight >= boxPosition) {
+    box.classList.add(animation)
+    box.classList.remove(`${animation}-opposite`)
+  }
 })
+
+let slider = document.querySelector('#myRange')
+slider.addEventListener('input', () => {
+  let sliderValue = slider.value
+  let earthquake = document.querySelector('#earthquake')
+  earthquake.innerHTML = `@keyframes earthquake {
+    0% {
+      transform: translateX(${sliderValue}px)
+    }
+    100% {
+      transform: translateX(-${sliderValue}px)
+    }
+  }`
+})
+
+let box2 = document.querySelector('#box2')
